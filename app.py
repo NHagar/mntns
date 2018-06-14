0651928ee92e6ef4639449a38b064cb16c1aa6dc
Nicholas Hagar <nicholashagar@Nicholass-MacBook-Air-2.local>
2018-06-13 23:41:47 -0500

    horu

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def send():
             % (response.status_code, response.text)
     )
 
-if datetime.datetime.now().hour == 23:
+if datetime.datetime.now().hour == 0:
     database()
     send()
 else:
